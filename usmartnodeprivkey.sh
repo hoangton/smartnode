a9361b8945348ae5d7e2743e1c3b3c45d967660e
hoangton <smartcash.cc@gmail.com>
2019-02-27 16:49:51 +0700

    Delete usmartnodeprivkey.sh

--- usmartnodeprivkey.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# usmartnodeprivkey.sh
-# Get a new privatekey by going to console >> debug and typing smartnode genkey
-printf "SmartNode GenKey: "
-read _nodePrivateKey
-cd && cd .smartcash
-sed -i "s/^\(smartnodeprivkey\s*=\s*\).*\$/\1$_nodePrivateKey/"  smartcash.conf
