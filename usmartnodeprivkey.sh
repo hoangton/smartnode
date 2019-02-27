fd550b4ea0c42ac2a1a67ad1f6db39cb94fa1433
hoangton <smartcash.cc@gmail.com>
2019-02-27 16:33:29 +0700

    Update usmartnodeprivkey.sh

--- usmartnodeprivkey.sh
+++ usmartnodeprivkey.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # usmartnodeprivkey.sh
-cd && cd .smartcash
+cd 
+cd .smartcash
 # Get a new privatekey by going to console >> debug and typing smartnode genkey
 printf "SmartNode GenKey: "
 read _nodePrivateKey
